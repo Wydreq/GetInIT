8a4a748c975ea52dcad4e859b0ff919602c0fbf6
gembele <92727369+gembele@users.noreply.github.com>
2023-06-17 22:28:02 +0200

    TEST: ButtonsContainer

---
+++ getinit/src/components/__tests__/ButtonsContainer.test.js
@@ -0,0 +1,136 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useNavigate } from 'react-router-dom';
+import ButtonsContainer from '../companyPanel/ButtonsContainer';
+
+jest.mock('react-router-dom', () => ({
+  useNavigate: jest.fn(),
+}));
+
+describe('Buttons Container', () => {
+  test('renders "Add manual payment" button for Admin role', () => {
+    const mockNavigate = jest.fn();
+    useNavigate.mockReturnValue(mockNavigate);
+
+    const userInfo = {
+      role: 'Admin',
+    };
+
+    render(<ButtonsContainer userInfo={userInfo} />);
+    const addPaymentButton = screen.getByRole('button-add');
+
+    expect(addPaymentButton).toBeInTheDocument();
+
+    userEvent.click(addPaymentButton);
+    expect(mockNavigate).toHaveBeenCalledWith('/manualPayment', {
+      state: {
+        userRole: 'Admin',
+      },
+    });
+  });
+  
+  test('renders "You Job Applications" button', () => {
+    const mockNavigate = jest.fn();
+    useNavigate.mockReturnValue(mockNavigate);
+
+    const userInfo = {
+      role: 'UserAccount',
+    };
+
+    render(<ButtonsContainer userInfo={userInfo} />);
+    const viewApplicationsButton = screen.getByRole('button-applications');
+
+    expect(viewApplicationsButton).toBeInTheDocument();
+
+    userEvent.click(viewApplicationsButton);
+    expect(mockNavigate).toHaveBeenCalledWith('/userApplications', {
+        state: {
+            userRole: 'UserAccount',
+        }
+    });
+  });
+
+  test('renders "Manage Your Offers(employee)" button', () => {
+    const mockNavigate = jest.fn();
+    useNavigate.mockReturnValue(mockNavigate);
+
+    const userInfo = {
+      role: 'EmployeeAccount',
+    };
+
+    render(<ButtonsContainer userInfo={userInfo} />);
+    const manageYourOffersEmployeeButton = screen.getByRole('button-offers-employee');
+
+    expect(manageYourOffersEmployeeButton).toBeInTheDocument();
+
+    userEvent.click(manageYourOffersEmployeeButton);
+    expect(mockNavigate).toHaveBeenCalledWith('/userOffers', {
+        state: {
+            userRole: 'EmployeeAccount',
+        }
+    });
+  });
+
+  test('renders "Manage Your Offers(manager)" button', () => {
+    const mockNavigate = jest.fn();
+    useNavigate.mockReturnValue(mockNavigate);
+
+    const userInfo = {
+      role: 'ManagerCompanyAccount',
+    };
+
+    render(<ButtonsContainer userInfo={userInfo} />);
+    const manageYourOffersManagerButton = screen.getByRole('button-offers-manager');
+
+    expect(manageYourOffersManagerButton).toBeInTheDocument();
+
+    userEvent.click(manageYourOffersManagerButton);
+    expect(mockNavigate).toHaveBeenCalledWith('/userOffers', {
+        state: {
+            userRole: 'ManagerCompanyAccount',
+        }
+    });
+  });
+  test('renders "Manage Your Company Accounts" button', () => {
+    const mockNavigate = jest.fn();
+    useNavigate.mockReturnValue(mockNavigate);
+
+    const userInfo = {
+      role: 'ManagerCompanyAccount',
+    };
+
+    render(<ButtonsContainer userInfo={userInfo} />);
+    const manageYourAccountsButton = screen.getByRole('button-accounts');
+
+    expect(manageYourAccountsButton).toBeInTheDocument();
+
+    userEvent.click(manageYourAccountsButton);
+    expect(mockNavigate).toHaveBeenCalledWith('/companyAccounts');
+  });
+
+  test('renders "Edit account info" button', () => {
+    const mockNavigate = jest.fn();
+    useNavigate.mockReturnValue(mockNavigate);
+
+    const userInfo = {
+      role: 'UserAccount',
+    };
+
+    render(<ButtonsContainer userInfo={userInfo} />);
+    const editInfoButton = screen.getByRole('button-edit');
+
+    expect(editInfoButton).toBeInTheDocument();
+
+    userEvent.click(editInfoButton);
+    expect(mockNavigate).toHaveBeenCalledWith('/editInfo');
+  });
+
+  
+
+  test('renders "Delete your account" button', () => {
+    render(<ButtonsContainer userInfo={{}} />);
+    const deleteAccountButton = screen.getByRole('button-delete');
+
+    expect(deleteAccountButton).toBeInTheDocument();
+  });
+});
--- getinit/src/components/companyPanel/ButtonsContainer.js
+++ getinit/src/components/companyPanel/ButtonsContainer.js
@@ -7,29 +7,29 @@ const ButtonsContainer = (props) => {
     return (
         <>
         <div className={classes.container}>
-            {props.userInfo.role === 'Admin' && <div className={classes.button} onClick={()=>{navigate('/manualPayment', {
+            {props.userInfo.role === 'Admin' && <div className={classes.button} role='button-add' onClick={()=>{navigate('/manualPayment', {
                 state: {
                     userRole: props.userInfo.role,
                 }
             })}}>Add manual payment</div>}
-            {props.userInfo.role === 'UserAccount' && <div className={classes.button} onClick={()=>{navigate('/userApplications', {
+            {props.userInfo.role === 'UserAccount' && <div className={classes.button} role='button-applications' onClick={()=>{navigate('/userApplications', {
                 state: {
                     userRole: props.userInfo.role,
                 }
             })}}>Your job applications</div>}
-            {props.userInfo.role === 'ManagerCompanyAccount' && <div className={classes.button} onClick={()=>{navigate('/userOffers', {
+            {props.userInfo.role === 'ManagerCompanyAccount' && <div className={classes.button} role='button-offers-manager' onClick={()=>{navigate('/userOffers', {
                 state: {
                     userRole: props.userInfo.role,
                 }
             })}}>Manage your offers</div>}
-            {props.userInfo.role === 'EmployeeAccount' && <div className={classes.button} onClick={()=>{navigate('/userOffers', {
+            {props.userInfo.role === 'EmployeeAccount' && <div className={classes.button} role='button-offers-employee' onClick={()=>{navigate('/userOffers', {
                 state: {
                     userRole: props.userInfo.role,
                 }
             })}}>Manage your offers</div>}
-            <div className={classes.button} onClick={()=>{navigate('/editInfo')}}>Edit account info</div>
-            {props.userInfo.role === 'ManagerCompanyAccount' && <div className={classes.button} onClick={()=>{navigate('/companyAccounts');}}>Manage your company accounts</div>}
-            <div className={classes.button}>Delete your account</div>
+            <div className={classes.button} role='button-edit' onClick={()=>{navigate('/editInfo')}}>Edit account info</div>
+            {props.userInfo.role === 'ManagerCompanyAccount' && <div className={classes.button} role='button-accounts' onClick={()=>{navigate('/companyAccounts');}}>Manage your company accounts</div>}
+            <div className={classes.button}role='button-delete'>Delete your account</div>
         </div>
         </>
     )
